ceffa0b39f8a3ce7bcd6d4176f0dd41f3e5adfa4
Daniel Santos <daniel@brasileiro.ca>
2018-02-19 01:56:50 -0500

    Creates Todo class and adds get-set to all properties

---
+++ WcfService-Todo-Demo/WcfServiceTodo/Todo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WcfServiceTodo
+{
+    public class Todo
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Tags
+        {
+            get;
+            set;
+        }
+
+        public string Author
+        {
+            get;
+            set;
+        }
+
+        public string Executor
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateDue
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateDone
+        {
+            get;
+            set;
+        }
+    }
+}
