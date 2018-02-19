eae05315614ea908df440e23050a086b9866d408
Daniel Santos <daniel@brasileiro.ca>
2018-02-19 02:04:43 -0500

    Establish the contracts for the interface (CRUD)
    TODO: attention to DELETE (I believe it should get an id)

--- WcfService-Todo-Demo/WcfServiceTodo/IServiceTodo.cs
+++ WcfService-Todo-Demo/WcfServiceTodo/IServiceTodo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace WcfServiceTodo
@@ -12,6 +13,23 @@ namespace WcfServiceTodo
     public interface IServiceTodo
     {
         [OperationContract]
-        void DoWork();
+        [WebInvoke(Method = "GET", UriTemplate = "findall", RequestFormat = WebMessageFormat.Json)]
+        List<Todo> findAll();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "find/{id}", RequestFormat = WebMessageFormat.Json)]
+        Todo find(int id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "create", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        bool create(Todo todo);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "edit", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        bool edit(Todo todo);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "delete", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        bool delete(Todo todo);
     }
 }
